2fe4d402887f16f54e022cd8d65f1cb8fb57a6cb
Darien Sweitzer <43593384+darien-sweitzer@users.noreply.github.com>
2025-12-09 16:49:35 -0500

    Delete DungeonRAW directory

--- DungeonRAW/src/thedungeon/Abilities.java
+++
@@ -1,9 +0,0 @@
-package thedungeon;
-
-public class Abilities {
-
-    public String[] abilityName = new String[3];
-    public String[] abilityDesc = new String[3];
-    public int[] damage = new int[3];
-
-} // close class
