e6353ad93c1a5d49b26f090a13603b4b6edc5ff9
Darien Sweitzer <43593384+darien-sweitzer@users.noreply.github.com>
2025-12-09 16:54:59 -0500

    Dungeon Raw
    Dungeon Raw

---
+++ DungeonRAW/src/thedungeon/Abilities.java
@@ -0,0 +1,9 @@
+package thedungeon;
+
+public class Abilities {
+
+    public String[] abilityName = new String[3];
+    public String[] abilityDesc = new String[3];
+    public int[] damage = new int[3];
+
+} // close class
